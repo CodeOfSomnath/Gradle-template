a1a84f8718e9a6f2ddf9a748c8d7b66a8fbdf87c
CodeOfSomnath <123479436+CodeOfSomnath@users.noreply.github.com>
2023-05-06 05:09:39 +0000

    Little update

--- demo/app/src/main/java/demo/App.java
+++ demo/app/src/main/java/demo/App.java
@@ -1,22 +1,30 @@
-package demo;
+import java.io.File;
+import com.itextpdf.kernel.pdf.PdfDocument;
+import com.itextpdf.kernel.pdf.PdfWriter;
+import com.itextpdf.layout.Document;
+import com.itextpdf.layout.element.Paragraph;
+import com.itextpdf.html2pdf.HtmlConverter;
 
-import java.net.URI;
-import java.net.http.HttpClient;
-import java.net.http.HttpRequest;
-import java.net.http.HttpResponse;
+public class HtmlToPDFConverter {
 
-public class App {
-
-  public static void main(String[] args) throws Exception {
-    String url = "https://download.java.net/java/early_access/panama/docs/api/java.net.http/java/net/http/package-summary.html";
-    HttpClient client = HttpClient.newHttpClient();
-    HttpRequest request = HttpRequest.newBuilder()
-        .uri(URI.create(url))
-        .GET()
-        .build();
-
-    HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
-    System.out.println("Response code: " + response.statusCode());
-    System.out.println("Response body: " + response.body());
-  }
+   public static void main(String[] args) {
+      String inputFilePath = "input.html";
+      String outputFilePath = "output.pdf";
+      try {
+         // Create a PDF document object
+         PdfDocument pdfDoc = new PdfDocument(new PdfWriter(outputFilePath));
+         Document doc = new Document(pdfDoc);
+         
+         // Convert HTML file to PDF and add to the PDF document
+         File inputFile = new File(inputFilePath);
+         HtmlConverter.convertToPdf(inputFile, pdfDoc, doc);
+         
+         // Close the PDF document
+         doc.close();
+         pdfDoc.close();
+         System.out.println("PDF file created successfully!");
+      } catch (Exception e) {
+         e.printStackTrace();
+      }
+   }
 }
